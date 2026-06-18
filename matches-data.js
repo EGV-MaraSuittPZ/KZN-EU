bb9b27f571a78a5b8326359d089f1ff6c4774293
EGV-MaraSuitt_PZ <egvcij@gmail.com>
2026-06-18 11:17:35 +0200

    Reset match data for new entries

--- matches-data.js
+++ matches-data.js
@@ -8,45 +8,9 @@
 
 const matchesData = [
    {
-    opponent: "Euskal", opponentLogo: "Logos/euskal.png",
-    ourScore: 7, opponentScore: 5, status: "win",
-    league: "kom", leagueName: "King of Master",
-    leagueLogo: "kom.png", date: "16 MAY, 2026"
-  },
-  {
-    opponent: "ProdigiFive", opponentLogo: "Logos/p5.png",
-    ourScore: 0, opponentScore: 2, status: "loss",
-    league: "esp", leagueName: "Competición Española",
-    leagueLogo: "competicion_española.png", date: "17 MAY, 2026"
-  },
-  {
-    opponent: "S.O.T", opponentLogo: "Logos/sot.png",
-    ourScore: 7, opponentScore: 5, status: "win",
-    league: "esp", leagueName: "Competición Española",
-    leagueLogo: "competicion_española.png", date: "9 MAY, 2026"
-  },
-  {
-    opponent: "(FW)ProdigiFive", opponentLogo: "Logos/p5.png",
-    ourScore: 0, opponentScore: 7, status: "loss",
-    league: "esp", leagueName: "Competición Española",
-    leagueLogo: "competicion_española.png", date: "1 MAY, 2026"
-  },
-  {
-    opponent: "SCRAV", opponentLogo: "Logos/scrav.png",
-    ourScore: 7, opponentScore: 4, status: "win",
-    league: "esp", leagueName: "Competición Española",
-    leagueLogo: "competicion_española.png", date: "25 ABR, 2026"
-  },
-  {
-    opponent: "YoungCracks", opponentLogo: "Logos/yn.png",
-    ourScore: 8, opponentScore: 6, status: "win",
-    league: "esp", leagueName: "Competición Española",
-    leagueLogo: "competicion_española.png", date: "19 ABR, 2026"
-  },
-  {
-    opponent: "UDR", opponentLogo: "Logos/udr.png",
-    ourScore: 7, opponentScore: 0, status: "win",
-    league: "esp", leagueName: "Competición Española",
-    leagueLogo: "competicion_española.png", date: "12 ABR, 2026"
+    opponent: "", opponentLogo: "",
+    ourScore: , opponentScore: , status: "",
+    league: "", leagueName: "",
+    leagueLogo: "", date: ""
   },
 ];
