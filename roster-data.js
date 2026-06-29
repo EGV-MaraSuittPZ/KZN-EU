cc8335df64e104197580f074c631937b136a434c
EGV-MaraSuitt_PZ <egvcij@gmail.com>
2026-06-29 09:47:48 +0200

    Update roster-data.js

--- roster-data.js
+++ roster-data.js
@@ -10,16 +10,16 @@
 
 const rosterData = [
   // ── JUGADORES ──────────────────────────────────────────
-  { name: "",   role: "jugador", position: "Capitan",     photo: "", twitter: "" },
-  { name: "",   role: "jugador", position: "Sub-Capitan", photo: "", twitter: "" },
-  { name: "qSxntxxx",      role: "jugador", position: "Jugador",     photo: "", twitter: "" },
-  { name: "",     role: "jugador", position: "Jugador",     photo: "", twitter: "" },
-  { name: "",       role: "jugador", position: "Jugador",     photo: "", twitter: "" },
-  { name: "",   role: "jugador", position: "Jugador",     photo: "", twitter: "" },
-  { name: "",  role: "jugador", position: "Jugador",     photo: "", twitter: "" },
+  { name: "MxzanKZN",   role: "jugador", position: "Capitan",     photo: "", twitter: "" },
+  { name: "JotaBe98",   role: "jugador", position: "Sub-Capitan", photo: "", twitter: "" },
+  { name: "inmakevin",      role: "jugador", position: "Jugador",     photo: "", twitter: "" },
+  { name: "ShxdoKZN",     role: "jugador", position: "Jugador",     photo: "", twitter: "" },
+  { name: "FBI_Ian_07",       role: "jugador", position: "Jugador",     photo: "", twitter: "" },
+  { name: "Useer00_",   role: "jugador", position: "Jugador",     photo: "", twitter: "" },
+  { name: "NxbyKZN",  role: "jugador", position: "Jugador",     photo: "", twitter: "" },
   { name: "retwrr",        role: "jugador", position: "Jugador",     photo: "", twitter: "" },
   { name: "rczxo",         role: "jugador", position: "Jugador",     photo: "", twitter: "" },
-  { name: "",       role: "jugador", position: "Jugador",     photo: "", twitter: "" },
+  { name: "xRweeze",       role: "jugador", position: "Jugador",     photo: "", twitter: "" },
 
   // ── DIRECCIÓN ──────────────────────────────────────────
   { name: "CxpeteeKNZ",   role: "staff",   position: "CEO",         photo: "", twitter: "" },
