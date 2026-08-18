1c7bc83cc23a09633c1df398ea46ab45e671724e
EGV-MaraSuitt_PZ <egvcij@gmail.com>
2026-08-18 15:40:41 +0000

    Refactor matchesData structure and fix status
    Removed duplicate matchesData entries and corrected status capitalization.

--- matches-data.js
+++ matches-data.js
@@ -9,49 +9,39 @@
 const matchesData = [
    {
     opponent: "YoungCracks🇪🇸 (j1)", opponentLogo: "Logos/yn.png",
-    ourScore: 1, opponentScore: 2, status: "Loss",
+    ourScore: 1, opponentScore: 2, status: "loss",
     league: "?", leagueName: "Ethenal EU ",
     leagueLogo: "Logos/ethlegue.png", date: "20/07/2026"
   },
-];
-
-const matchesData = [
+   
    {
     opponent: "X5 🇫🇷 (j2)", opponentLogo: "Logos/X5.png",
     ourScore: 2, opponentScore: 0, status: "win",
     league: "?", leagueName: "Ethenal EU ",
     leagueLogo: "Logos/ethlegue.png", date: "27/07/2026"
   },
-];
-
-const matchesData = [
+   
    {
     opponent: "MafiaWars 🇮🇹🇵🇹 (j3)", opponentLogo: "Logos/mafia.png",
     ourScore: 2, opponentScore: 0, status: "win",
     league: "?", leagueName: "Ethenal EU ",
     leagueLogo: "Logos/ethlegue.png", date: "3/08/2026"
   },
-];
 
-const matchesData = [
    {
     opponent: "Scrav🇪🇸 (quarterF)", opponentLogo: "Logos/scrav.png",
     ourScore: 2, opponentScore: 0, status: "win",
     league: "?", leagueName: "Ethenal EU ",
     leagueLogo: "Logos/ethlegue.png", date: "10/08/2026"
   },
-   
-];
-const matchesData = [
+
    {
     opponent: "Wars🇫🇷🇦🇪 (semiF)", opponentLogo: "Logos/wars.png",
     ourScore: 2, opponentScore: 1, status: "win",
     league: "?", leagueName: "Ethenal EU ",
     leagueLogo: "Logos/ethlegue.png", date: "27/07/2026"
   },
-];
 
-const matchesData = [
    {
     opponent: "ZND (Final)", opponentLogo: "Logos/znd.png",
     ourScore: 0, opponentScore: 0, status: "upcoming",
