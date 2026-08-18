3062748b02fd1c59a12c62b54b225d3637df32b7
EGV-MaraSuitt_PZ <egvcij@gmail.com>
2026-08-18 14:50:52 +0000

    Update matches-data.js

--- matches-data.js
+++ matches-data.js
@@ -8,9 +8,55 @@
 
 const matchesData = [
    {
+    opponent: "YoungCracks", opponentLogo: "Logos/X5.png",
+    ourScore: 2, opponentScore: 0, status: "win",
+    league: "?", leagueName: "Ethenal EU ",
+    leagueLogo: "Logos/ethlegue.png", date: "27/07/2026"
+  },
+];
+
+const matchesData = [
+   {
     opponent: "X5 🇫🇷", opponentLogo: "Logos/X5.png",
     ourScore: 2, opponentScore: 0, status: "win",
     league: "?", leagueName: "Ethenal EU ",
     leagueLogo: "Logos/ethlegue.png", date: "27/07/2026"
   },
 ];
+
+const matchesData = [
+   {
+    opponent: "MafiaWars", opponentLogo: "Logos/X5.png",
+    ourScore: 2, opponentScore: 0, status: "win",
+    league: "?", leagueName: "Ethenal EU ",
+    leagueLogo: "Logos/ethlegue.png", date: "27/07/2026"
+  },
+];
+
+const matchesData = [
+   {
+    opponent: "Scrav", opponentLogo: "Logos/X5.png",
+    ourScore: 2, opponentScore: 0, status: "win",
+    league: "?", leagueName: "Ethenal EU ",
+    leagueLogo: "Logos/ethlegue.png", date: "27/07/2026"
+  },
+   
+];
+const matchesData = [
+   {
+    opponent: "Wars", opponentLogo: "Logos/X5.png",
+    ourScore: 2, opponentScore: 0, status: "win",
+    league: "?", leagueName: "Ethenal EU ",
+    leagueLogo: "Logos/ethlegue.png", date: "27/07/2026"
+  },
+];
+
+const matchesData = [
+   {
+    opponent: "ZND", opponentLogo: "Logos/X5.png",
+    ourScore: 2, opponentScore: 0, status: "win",
+    league: "?", leagueName: "Ethenal EU ",
+    leagueLogo: "Logos/ethlegue.png", date: "27/07/2026"
+  },
+];
+
