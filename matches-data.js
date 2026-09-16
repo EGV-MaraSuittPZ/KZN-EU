cc1e6cab66319da985e6793da45d91528ddc4083
EGV-MaraSuitt_PZ <egvcij@gmail.com>
2026-09-16 10:12:37 +0200

    Update matches-data.js

--- matches-data.js
+++ matches-data.js
@@ -12,56 +12,56 @@ const matchesData = [
      {
     opponent: "Vora", opponentLogo: "Logos/yn.png",
     ourScore: 7, opponentScore: 0, status: "win",
-    league: "?", leagueName: "Scrim",
+    league: "Scrim", leagueName: "Scrim",
     leagueLogo: "", date: "13/08/2026"
   },
    
    {
     opponent: "ZND (Final)", opponentLogo: "Logos/znd.png",
     ourScore: 2, opponentScore: 3, status: "loss",
-    league: "?", leagueName: "Ethenal EU ",
+    league: "Ethenal EU", leagueName: "Ethenal EU ",
     leagueLogo: "Logos/ethlegue.png", date: "22/08/2026"
   },
 
    {
     opponent: "Wars🇫🇷🇦🇪 (semiF)", opponentLogo: "Logos/wars.png",
     ourScore: 2, opponentScore: 1, status: "win",
-    league: "?", leagueName: "Ethenal EU ",
+    league: "Ethenal EU", leagueName: "Ethenal EU ",
     leagueLogo: "Logos/ethlegue.png", date: "17/08/2026"
   },
 
     {
     opponent: "Scrav🇪🇸 (quarterF)", opponentLogo: "Logos/scrav.png",
     ourScore: 2, opponentScore: 0, status: "win",
-    league: "?", leagueName: "Ethenal EU ",
+    league: "Ethenal EU", leagueName: "Ethenal EU ",
     leagueLogo: "Logos/ethlegue.png", date: "10/08/2026"
   },
 
     {
     opponent: "MafiaWars 🇮🇹🇵🇹 (j3)", opponentLogo: "Logos/mafia.png",
     ourScore: 2, opponentScore: 0, status: "win",
-    league: "?", leagueName: "Ethenal EU ",
+    league: "Ethenal EU", leagueName: "Ethenal EU ",
     leagueLogo: "Logos/ethlegue.png", date: "3/08/2026"
   },
    
     {
     opponent: "X5 🇫🇷 (j2)", opponentLogo: "Logos/X5.png",
     ourScore: 2, opponentScore: 0, status: "win",
-    league: "?", leagueName: "Ethenal EU ",
+    league: "Ethenal EU", leagueName: "Ethenal EU ",
     leagueLogo: "Logos/ethlegue.png", date: "27/07/2026"
   },
    
    {
     opponent: "YoungCracks🇪🇸 (j1)", opponentLogo: "Logos/yn.png",
     ourScore: 1, opponentScore: 2, status: "loss",
-    league: "?", leagueName: "Ethenal EU ",
+    league: "Ethenal EU", leagueName: "Ethenal EU ",
     leagueLogo: "Logos/ethlegue.png", date: "20/07/2026"
   },
    
    {
     opponent: "X5 🇫🇷 (j2)", opponentLogo: "Logos/X5.png",
     ourScore: 2, opponentScore: 0, status: "win",
-    league: "?", leagueName: "Ethenal EU ",
+    league: "Ethenal EU", leagueName: "Ethenal EU ",
     leagueLogo: "Logos/ethlegue.png", date: "27/07/2026"
   },
    
