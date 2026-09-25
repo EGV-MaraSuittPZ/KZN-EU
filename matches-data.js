8e7a2797c82c4ddbeee7a6dd6f6d98e29c24eee7
EGV-MaraSuitt_PZ <egvcij@gmail.com>
2026-09-25 09:58:41 +0200

    Fix opponentScore value in matches-data.js

--- matches-data.js
+++ matches-data.js
@@ -10,7 +10,7 @@ const matchesData = [
 
   {
     opponent: "kgjs", opponentLogo: "Logos/.png",
-    ourScore: 7, opponentScore: , status: "win",
+    ourScore: 7, opponentScore: 2, status: "win",
     league: "Scrim", leagueName:2 "Scrim",
     leagueLogo: "", date: "22/09/2026"
   },
